8dc6099ccaa5a6c90aca017f84512e899a58b047
Darsonn <87154431+Darsonn@users.noreply.github.com>
2026-05-21 22:11:51 +0200

    Initial commit

---
+++ src/main/java/pl/darsonn/nominalapi/NominalApiApplication.java
@@ -0,0 +1,13 @@
+package pl.darsonn.nominalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NominalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/darsonn/nominalapi/NominalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.darsonn.nominalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
